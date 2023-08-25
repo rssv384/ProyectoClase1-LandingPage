ae0f612269154cf7ef9e8b65f0f9ac18dbc7c2b1
Raúl Soto <rssotovejar@gmail.com>
2023-08-24 21:02:58 -0700

    Botón flotante, headings, home banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('Hola Mundo');
 
+// Mostrar alerta al dar click en el logo
 function mostrarMensajeInicial() {
 	if (confirm('Este sitio está en desarrollo, disculpe las molestias.')) {
 		alert('Gracias por visitarnos!');
@@ -7,3 +8,23 @@ function mostrarMensajeInicial() {
 		alert('...');
 	}
 }
+
+// Funcionalidad botón flotante
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollToTopButton = document.getElementById('btn-ir-arriba');
+
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 100) {
+			scrollToTopButton.style.display = 'block';
+		} else {
+			scrollToTopButton.style.display = 'none';
+		}
+	});
+
+	scrollToTopButton.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	});
+});
